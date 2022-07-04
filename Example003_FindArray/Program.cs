5f1a0248b65406f6f99bc8786062b324b30ea03d
DemionVigovsky <morrow130790130@gmail.com>
2022-07-04 18:48:57 +0300

    Поиск числа в массиве

---
+++ Example003_FindArray/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = {1,232,7,124,5,566,7,18};
+
+int N = array.Length;
+int find = 7;
+
+int index = 0;
+
+while (index < N)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
