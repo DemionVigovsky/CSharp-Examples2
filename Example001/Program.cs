a16bced1febe143f6cb99f102edb5011ce3921ee
DemionVigovsky <morrow130790130@gmail.com>
2022-07-04 18:02:03 +0300

    Сделали функцию поиска максимума

--- Example001/Program.cs
+++ Example001/Program.cs
@@ -9,7 +9,7 @@
 
 int a1 = 1;
 int b1 = 224;
-int c1 = 3;
+int c1 = 33113;
 int a2 = 11;
 int b2 = 162;
 int c2 = 13;
@@ -17,9 +17,14 @@ int a3 = 21;
 int b3 = 1222;
 int c3 = 23;
 
-int max1 = Max(a1,b1,c1);
-int max2 = Max(a2,b2,c2);
-int max3 = Max(a3,b3,c3);
-int max = Max(max1, max2, max3);
+// int max1 = Max(a1,b1,c1);
+// int max2 = Max(a2,b2,c2);
+// int max3 = Max(a3,b3,c3);
+// int max = Max(max1, max2, max3);
+
+int max = Max(
+    Max(a1,b1,c1),
+    Max(a2,b2,c2),
+    Max(a3,b3,c3));
 
 Console.WriteLine(max);
